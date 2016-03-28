32e7bf75908da212b25a8ff56bcc5ca05c12891a
Marc <marc.izquierdo.hu@gmail.com>
2016-03-28 19:42:22 +0200

    Added PlatformTimer.h

---
+++ DeltaEngine/include/PlatformTimer.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "types.h"
+
+namespace DeltaEngine {
+	namespace Internal {
+
+#if defined(_WIN32)
+#	ifdef _WINDOWS_
+#		undef _WINDOWS_
+#	endif
+#include <Windows.h>
+
+		static __forceinline Types::uint64 getTimerFrequency()
+		{
+			LARGE_INTEGER frequency;
+			QueryPerformanceFrequency(&frequency);
+
+			return frequency.QuadPart;
+		}
+
+		static __forceinline Types::uint64 getCurrentCount()
+		{
+			LARGE_INTEGER count;
+			QueryPerformanceCounter(&count);
+
+			return count.QuadPart;
+		}
+
+#elif defined(__unix__) || defined(__GNUC__) || defined(__GNUG__)
+#include <unistd.h>
+
+		static __forceinline Types::uint64 rdtsc()
+		{
+			Types::uint32 lo, hi;
+
+			__asm__ __volatile__("rdtsc" : "=a" (lo), "=d" (hi));
+
+			return (Types::uint64)hi << 32 | lo;
+		}
+
+		static __forceinline Types::uint64 getTimerFrequency()
+		{
+			Types::uint64 before, after;
+
+			before = rdtsc();
+			usleep(1000000); //10^6 microseconds = 1 second
+			after = rdtsc();
+
+			return (after - before);
+		}
+
+		static __forceinline Types::uint64 getCurrentCount()
+		{
+			return rdtsc();
+		}
+
+#else
+#error This platform is not supported!
+#endif
+
+	}
+}
